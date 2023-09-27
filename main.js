253c74f36f5a9c977d851b104b2f2e8a557fb605
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-27 16:01:09 +0900

    cubeの変数を作成し、geometryとmaterialを入れる

--- main.js
+++ main.js
@@ -19,3 +19,4 @@ document.body.appendChild(renderer.domElement);
 /* ボックスのサイズ決定、メッシュ、追加 */
 const geometry = new THREE.BOXGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
+const cube = new THREE.Mesh(geometry, material);
