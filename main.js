2e1fa78a790fe339188817105af8ee74eccef9b5
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-27 16:02:10 +0900

    sceneにcubeを追加する

--- main.js
+++ main.js
@@ -20,3 +20,4 @@ document.body.appendChild(renderer.domElement);
 const geometry = new THREE.BOXGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
 const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
