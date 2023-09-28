525c88c2a02b9b4225f042765b7968c267142be5
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-28 13:40:34 +0900

    typo修正

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 /* ボックスのサイズ決定、メッシュ、追加 */
-const geometry = new THREE.BOXGeometry(2, 2, 2);
+const geometry = new THREE.BoxGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
