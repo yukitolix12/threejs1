99343d198b99da9178021e1a62de0bf9478a989b
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-28 14:04:31 +0900

    初期化の準備

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+let scene, camera, renderer, cube;
+
+function init(){
 /* シーン */
-const scene = new THREE.Scene();
+ scene = new THREE.Scene();
 
 /* カメラ */
-const camera = new THREE.PerspectiveCamera(
+ camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 );
+ 
 
 /* レンダラー */
-const renderer = new THREE.WebGLRenderer();
+ renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -19,10 +23,11 @@ document.body.appendChild(renderer.domElement);
 /* ボックスのサイズ決定、メッシュ、追加 */
 const geometry = new THREE.BoxGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
-const cube = new THREE.Mesh(geometry, material);
+cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+}
 
 /* アニメーション制御 */
 function animate() {
